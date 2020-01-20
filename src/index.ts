f4a3d56ff9c6a41ffc04a50e70609da4463b9f4e
yiminghe <yiminghe@gmail.com>
2020-01-20 23:56:51 +0800

    optimize

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 // https://stackoverflow.com/questions/9452701/ukkonens-suffix-tree-algorithm-in-plain-english
 
-const LIMIT = 1<<30;
+const LIMIT = 1 << 30;
+let id = 0;
 
 interface STNext {
-  [o: string]: number;
+  [o: string]: STNode;
 }
 
 class STNode {
@@ -11,14 +12,16 @@ class STNode {
   end: number;
   next: STNext;
   tree: SuffixTee;
-  link: number;
+  link?: STNode;
+  id: number;
 
   constructor(start: number, end: number) {
     this.start = start;
     this.end = end;
-    this.next = {};
+    this.next = Object.create(null);
     this.tree = null as any;
-    this.link = 0;
+    this.link = undefined;
+    this.id = ++id;
   }
 
   getEdgeLength(): number {
@@ -27,34 +30,26 @@ class STNode {
 }
 
 class SuffixTee {
-  s: string;
-  nodes: STNode[];
-  root: number;
+  s: string[];
+  root: STNode;
   position: number;
-  currentNode: number;
-  needSuffixLink: number;
+  linkNode?: STNode;
   remainder: number;
-  active_node: number;
-  active_length: number;
-  active_edge: number;
+  activeNode: STNode;
+  activeLength: number;
+  activeEdge: number;
 
-  constructor(str:string) {
-    const len = str.length;
-    this.s = '';
-    this.nodes = new Array(2 * len + 2);
-    this.root = 0;
+  constructor(str: string) {
+    this.s = new Array(str.length);
     this.position = -1;
-    this.currentNode = 0;
-    this.needSuffixLink = 0;
+    this.linkNode = undefined;
     this.remainder = 0;
+    this.activeLength = 0;
+    this.activeEdge = 0;
 
-    this.active_node = 0;
-    this.active_length = 0;
-    this.active_edge = 0;
+    this.activeNode = this.root = this.newNode(-1, -1);
 
-    this.active_node = this.root = this.newNode(-1, -1);
-
-    for(const c of str){
+    for (const c of str) {
       this.addChar(c);
     }
   }
@@ -62,71 +57,79 @@ class SuffixTee {
   newNode(start: number, end: number) {
     const n = new STNode(start, end);
     n.tree = this;
-    this.nodes[++this.currentNode] = n;
-    return this.currentNode;
+    return n;
   }
 
-  addSuffixLink(node: number) {
-    if (this.needSuffixLink > 0)
-      this.nodes[this.needSuffixLink].link = node;
-    this.needSuffixLink = node;
+  addSuffixLink(node: STNode) {
+    if (this.linkNode) {
+      this.linkNode.link = node;
+    }
+    this.linkNode = node;
   }
 
   getActiveEdge() {
-    return this.s[this.active_edge];
+    return this.s[this.activeEdge];
   }
 
-  walkDown(next: number) {
-    const { active_length, nodes } = this;
-    if (active_length >= nodes[next].getEdgeLength()) {
-      this.active_edge += nodes[next].getEdgeLength();
-      this.active_length -= nodes[next].getEdgeLength();
-      this.active_node = next;
+  walkDown(next: STNode) {
+    const { activeLength } = this;
+    const nextEdgeLength = next.getEdgeLength();
+    if (activeLength >= nextEdgeLength) {
+      this.activeEdge += nextEdgeLength;
+      this.activeLength -= nextEdgeLength;
+      this.activeNode = next;
       return true;
     }
     return false;
   }
 
   addChar(c: string) {
-    const { nodes } = this;
-    ++this.position;
-    this.s += c;
-    this.needSuffixLink = -1;
+    this.s[++this.position] = c;
+    this.linkNode = undefined;
     this.remainder++;
     while (this.remainder > 0) {
-      if (this.active_length == 0) this.active_edge = this.position;
-      if (!nodes[this.active_node].next.hasOwnProperty(this.getActiveEdge())) {
+      if (this.activeLength == 0) {
+        this.activeEdge = this.position;
+      }
+      if (!this.activeNode.next[this.getActiveEdge()]) {
         let leaf = this.newNode(this.position, LIMIT);
-        nodes[this.active_node].next[this.getActiveEdge()] = leaf;
-        this.addSuffixLink(this.active_node); //rule 2
+        this.activeNode.next[this.getActiveEdge()] = leaf;
+        this.addSuffixLink(this.activeNode); //rule 2
       } else {
-        let next = nodes[this.active_node].next[this.getActiveEdge()];
-        if (this.walkDown(next)) continue;   //observation 2
-        if (this.s[nodes[next].start + this.active_length] == c) { //observation 1
-          this.active_length++;
-          this.addSuffixLink(this.active_node); // observation 3
+        let next = this.activeNode.next[this.getActiveEdge()];
+        if (this.walkDown(next)) {
+          continue; //observation 2
+        }
+        if (this.s[next.start + this.activeLength] === c) {
+          //observation 1
+          this.activeLength++;
+          this.addSuffixLink(this.activeNode); // observation 3
           break;
         }
-        let split = this.newNode(nodes[next].start, nodes[next].start + this.active_length);
-        nodes[this.active_node].next[this.getActiveEdge()] = split;
+        let split = this.newNode(next.start, next.start + this.activeLength);
+        this.activeNode.next[this.getActiveEdge()] = split;
         let leaf = this.newNode(this.position, LIMIT);
-        nodes[split].next[c] = leaf;
-        nodes[next].start += this.active_length;
-        nodes[split].next[this.s[nodes[next].start]] = next;
+        split.next[c] = leaf;
+        next.start += this.activeLength;
+        split.next[this.s[next.start]] = next;
         this.addSuffixLink(split); //rule 2
       }
       this.remainder--;
-      if (this.active_node == this.root && this.active_length > 0) {  //rule 1
-        this.active_length--;
-        this.active_edge = this.position - this.remainder + 1;
+      if (this.activeNode == this.root && this.activeLength > 0) {
+        //rule 1
+        this.activeLength--;
+        this.activeEdge = this.position - this.remainder + 1;
       } else
-        this.active_node = nodes[this.active_node].link > 0 ? nodes[this.active_node].link : this.root; //rule 3
+        this.activeNode = this.activeNode.link
+          ? this.activeNode.link
+          : this.root; //rule 3
     }
   }
 
-  edgeString(node: number) {
-    const nodes = this.nodes;
-    return this.s.slice(nodes[node].start, Math.min(this.position + 1, nodes[node].end));
+  edgeString(node: STNode) {
+    return this.s
+      .slice(node.start, Math.min(this.position + 1, node.end))
+      .join('');
   }
 
   printTree() {
@@ -135,7 +138,9 @@ class SuffixTee {
     out.push('digraph {');
     out.push('\trankdir = LR;');
     out.push('\tedge [arrowsize=0.4,fontsize=10]');
-    out.push('\tnode1 [label="",style=filled,fillcolor=lightgrey,shape=circle,width=.1,height=.1];');
+    out.push(
+      '\tnode1 [label="",style=filled,fillcolor=lightgrey,shape=circle,width=.1,height=.1];',
+    );
     out.push('//------leaves------');
     this.printLeaves(root, out);
     out.push('//------internal nodes------');
@@ -148,43 +153,56 @@ class SuffixTee {
     return out.join('\n');
   }
 
-  printLeaves(x: number, out: string[]) {
-    const { nodes } = this;
-    if (Object.keys(nodes[x].next).length == 0)
-      out.push('\tnode' + x + ' [label="",shape=point]');
-    else {
-      for (let child of Object.values(nodes[x].next))
+  printLeaves(x: STNode, out: string[]) {
+    if (Object.keys(x.next).length == 0) {
+      out.push('\tnode' + x.id + ' [label="",shape=point]');
+    } else {
+      for (let child of Object.values(x.next)) {
         this.printLeaves(child, out);
+      }
     }
   }
 
-  printInternalNodes(x: number, out: string[]) {
-    const { nodes } = this;
-    if (x != this.root && Object.keys(nodes[x].next).length > 0)
-      out.push('\tnode' + x + ' [label="",style=filled,fillcolor=lightgrey,shape=circle,width=.07,height=.07]');
-
-    for (let child of Object.values(nodes[x].next))
+  printInternalNodes(x: STNode, out: string[]) {
+    if (x != this.root && Object.keys(x.next).length > 0) {
+      out.push(
+        '\tnode' +
+          x.id +
+          ' [label="",style=filled,fillcolor=lightgrey,shape=circle,width=.07,height=.07]',
+      );
+    }
+    for (let child of Object.values(x.next)) {
       this.printInternalNodes(child, out);
+    }
   }
 
-  printEdges(x: number, out: string[]) {
-    const { nodes } = this;
-    for (let child of Object.values(nodes[x].next)) {
-      out.push('\tnode' + x + ' -> node' + child + ' [label="' + this.edgeString(child) + '",weight=3]');
+  printEdges(x: STNode, out: string[]) {
+    for (let child of Object.values(x.next)) {
+      out.push(
+        '\tnode' +
+          x.id +
+          ' -> node' +
+          child.id +
+          ' [label="' +
+          this.edgeString(child) +
+          '",weight=3]',
+      );
       this.printEdges(child, out);
     }
   }
 
-  printSLinks(x: number, out: string[]) {
-    const { nodes } = this;
-    if (nodes[x].link > 0)
-      out.push('\tnode' + x + ' -> node' + nodes[x].link + ' [label="",weight=1,style=dotted]');
-    for (let child of Object.values(nodes[x].next))
-      this.printSLinks(child, out);
+  printSLinks(x: STNode, out: string[]) {
+    if (x.link) {
+      out.push(
+        '\tnode' +
+          x.id +
+          ' -> node' +
+          x.link.id +
+          ' [label="",weight=1,style=dotted]',
+      );
+    }
+    for (let child of Object.values(x.next)) this.printSLinks(child, out);
   }
 }
 
 export default SuffixTee;
-
-
-
