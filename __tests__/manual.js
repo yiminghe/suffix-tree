c3e03e80ed7dcd239cbdfe47f90dc1d8c1c4da3a
yiminghe <yiminghe@gmail.com>
2020-01-21 17:50:12 +0800

    support maxCount/maxNode

--- __tests__/manual.js
+++ __tests__/manual.js
@@ -1,50 +1,74 @@
 import SuffixTee from '../src/';
 import fs from 'fs';
 import path from 'path';
+import now from 'performance-now';
 
-let s = 'abcdabc$';
+let s =
+  fs
+    .readFileSync(path.join(__dirname, 'data.txt'), {
+      encoding: 'utf-8',
+    })
+    .trim()
+    .slice(1, -1) + '$';
 let st = new SuffixTee(s);
+console.log(s.length);
 
 fs.writeFileSync(path.join(__dirname, 'test.dot'), st.printTree());
 
 console.log('done!');
 
-function dfs(n) {
-  const values = Object.values(n.next);
-  if (!values.length) {
-    return null;
-  }
-  let ret = {
-    max: n.getEdgeLength(),
-    end: n.getEnd(),
-  };
-  let max = -1;
-  let allNull = 1;
-  for (var c of values) {
-    let subRet = dfs(c);
-    if (subRet) {
-      allNull = 0;
-      if (subRet.max > max) {
-        max = subRet.max;
-        ret.end = subRet.end;
+(function() {
+  const start = now();
+  function dfs(n) {
+    const values = Object.values(n.next);
+    if (!values.length) {
+      return null;
+    }
+    let ret = {
+      max: n.getEdgeLength(),
+      end: n.getEnd(),
+    };
+    let max = -1;
+    let allNull = 1;
+    for (var c of values) {
+      let subRet = dfs(c);
+      if (subRet) {
+        allNull = 0;
+        if (subRet.max > max) {
+          max = subRet.max;
+          ret.end = subRet.end;
+        }
       }
     }
-  }
-  if (allNull) {
-    if (values.length >= 2) {
-      return ret;
+    if (allNull) {
+      if (values.length >= 2) {
+        return ret;
+      }
+      return null;
     }
-    return null;
+    ret.max += max;
+    return ret;
   }
-  ret.max += max;
-  return ret;
-}
 
-const ret = dfs(st.root);
+  const ret = dfs(st.root);
 
-if (!ret || ret.end === -1) {
-  console.log('no answer');
-} else {
-  const { max, end } = ret;
-  console.log(max, end, s.slice(end - max, end));
-}
+  if (!ret || ret.end === -1) {
+    console.log('no answer');
+  } else {
+    const { max, end } = ret;
+    console.log(max, end, s.slice(end - max, end));
+  }
+  console.log(now() - start);
+})();
+
+(function() {
+  const start = now();
+  const max = st.maxCount;
+  const end = st.maxNode && st.maxNode.getEnd();
+  if (max === -1) {
+    console.log('no answer');
+  } else {
+    console.log(max, end, s.slice(end - max, end));
+  }
+  console.log(now() - start);
+})();
--- src/index.ts
+++ src/index.ts
@@ -15,14 +15,18 @@ export class STNode {
   tree: SuffixTee;
   link?: STNode;
   id: number;
+  parent?: STNode;
+  count: number;
 
   constructor(start: number, end: number) {
     this.start = start;
     this.end = end;
     this.next = Object.create(null);
     this.tree = null as any;
-    this.link = undefined;
+    // this.link = undefined;
+    // this.parent = undefined;
     this.id = ++id;
+    this.count = 0;
   }
 
   getEdgeLength(): number {
@@ -38,6 +42,11 @@ export class STNode {
       .slice(this.start, this.start + this.getEdgeLength())
       .join('');
   }
+
+  addChild(c: string, node: STNode) {
+    this.next[c] = node;
+    node.parent = this;
+  }
 }
 
 class SuffixTee {
@@ -49,6 +58,8 @@ class SuffixTee {
   activeNode: STNode;
   activeLength: number;
   activeEdge: number;
+  maxCount: number;
+  maxNode?: STNode;
 
   constructor(str: string) {
     this.s = new Array(str.length);
@@ -57,7 +68,7 @@ class SuffixTee {
     this.remainder = 0;
     this.activeLength = 0;
     this.activeEdge = 0;
-
+    this.maxCount = -1;
     this.activeNode = this.root = this.newNode(-1, -1);
 
     for (const c of str) {
@@ -82,13 +93,13 @@ class SuffixTee {
     return this.s[this.activeEdge];
   }
 
-  walkDown(next: STNode) {
+  walkDown(nextNode: STNode) {
     const { activeLength } = this;
-    const nextEdgeLength = next.getEdgeLength();
+    const nextEdgeLength = nextNode.getEdgeLength();
     if (activeLength >= nextEdgeLength) {
       this.activeEdge += nextEdgeLength;
       this.activeLength -= nextEdgeLength;
-      this.activeNode = next;
+      this.activeNode = nextNode;
       return true;
     }
     return false;
@@ -105,25 +116,33 @@ class SuffixTee {
       const activeC = this.getActiveEdge();
       if (!this.activeNode.next[activeC]) {
         let leaf = this.newNode(this.position, LIMIT);
-        this.activeNode.next[activeC] = leaf;
+        this.activeNode.addChild(activeC, leaf);
         this.addSuffixLink(this.activeNode); //rule 2
       } else {
-        let next = this.activeNode.next[activeC];
-        if (this.walkDown(next)) {
+        let nextNode = this.activeNode.next[activeC];
+        if (this.walkDown(nextNode)) {
           continue; //observation 2
         }
-        if (this.s[next.start + this.activeLength] === c) {
+        if (this.s[nextNode.start + this.activeLength] === c) {
           //observation 1
           this.activeLength++;
           this.addSuffixLink(this.activeNode); // observation 3
           break;
         }
-        let split = this.newNode(next.start, next.start + this.activeLength);
-        this.activeNode.next[activeC] = split;
+        let split = this.newNode(
+          nextNode.start,
+          nextNode.start + this.activeLength,
+        );
+        this.activeNode.addChild(activeC, split);
         let leaf = this.newNode(this.position, LIMIT);
-        split.next[c] = leaf;
-        next.start += this.activeLength;
-        split.next[this.s[next.start]] = next;
+        split.addChild(c, leaf);
+        split.count += this.activeNode.count + split.getEdgeLength();
+        if (split.count > this.maxCount) {
+          this.maxCount = split.count;
+          this.maxNode = split;
+        }
+        nextNode.start += this.activeLength;
+        split.addChild(this.s[nextNode.start], nextNode);
         this.addSuffixLink(split); //rule 2
       }
       this.remainder--;
@@ -140,27 +159,6 @@ class SuffixTee {
 
   edgeString(node: STNode) {
     return this.s.slice(node.start, Math.min(this.s.length, node.end)).join('');
-  }
-
-  printTree() {
-    const out: string[] = [];
-    const root = this.root;
-    out.push('digraph {');
-    out.push('\trankdir = LR;');
-    out.push('\tedge [arrowsize=0.4,fontsize=10]');
-    out.push(
-      '\tnode1 [label="",style=filled,fillcolor=lightgrey,shape=circle,width=.1,height=.1];',
-    );
-    out.push('//------leaves------');
-    this.printLeaves(root, out);
-    out.push('//------internal nodes------');
-    this.printInternalNodes(root, out);
-    out.push('//------edges------');
-    this.printEdges(root, out);
-    out.push('//------suffix links------');
-    this.printSLinks(root, out);
-    out.push('}');
-    return out.join('\n');
   }
 
   printLeaves(x: STNode, out: string[]) {
@@ -212,6 +210,27 @@ class SuffixTee {
       );
     }
     for (let child of Object.values(x.next)) this.printSLinks(child, out);
+  }
+
+  printTree() {
+    const out: string[] = [];
+    const root = this.root;
+    out.push('digraph {');
+    out.push('\trankdir = LR;');
+    out.push('\tedge [arrowsize=0.4,fontsize=10]');
+    out.push(
+      '\tnode1 [label="",style=filled,fillcolor=lightgrey,shape=circle,width=.1,height=.1];',
+    );
+    out.push('//------leaves------');
+    this.printLeaves(root, out);
+    out.push('//------internal nodes------');
+    this.printInternalNodes(root, out);
+    out.push('//------edges------');
+    this.printEdges(root, out);
+    out.push('//------suffix links------');
+    this.printSLinks(root, out);
+    out.push('}');
+    return out.join('\n');
   }
 }
 
